7dca4911a1a7d1d73481a3e0a377d44792585482
Rosa Stampolidou <rosas@mkinet.net>
2019-03-19 15:08:07 +0200

    user input

--- skills/fromJSONdb_clean.js
+++ skills/fromJSONdb_clean.js
@@ -1,87 +1,151 @@
 const request = require('request');
 
 module.exports = function(controller) {
-    request('http://localhost/botkit/test_json/sample_db.json', function (error, response, body) {
-    var answerAdress;
-    var infoAddressArr = '';
+   // request('http://localhost/botkit/test_json/sample_db.json', function (error, response, body) {
+   request('https://api.myjson.com/bins/16uy1a', function (error, response, body) {
+   var answerAdress;
+   var newAnswer;
+   var infoAddressArr = '';
 
-        controller.hears('JSON Clean', 'message_received', function(bot, message) {
-            const info = JSON.parse(body);
-            // request('https://api.myjson.com/bins/ot27a', function (error, response, body) {
-            // request('http://localhost/botkit/test_json/sample_db.json', function (error, response, body) {
-            bot.startConversation(message, function(err, convo) {
+       controller.hears('JSON Clean', 'message_received', function(bot, message) {
+           const info = JSON.parse(body);
+           // request('http://localhost/botkit/test_json/sample_db.json', function (error, response, body) {
+           bot.startConversation(message, function(err, convo) {
+
+
+               console.log('error:', error); // Print the error if one occurred
+               console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+               // console.log('body:', body); // Print the HTML for the Google homepage.
+
+               // const info = JSON.parse(body);
+               convo.setVar('jsonInfo', info[0].Price);
+               // var infoAddressArr = '';
+               var infoPriceArr;
+
+
+               for(var i = 0; i < info.length; i++){
+
+                   infoAddressArr += info[i].Adress; //gia na tou deixnei ti exoume sti vasi
+                   convo.setVar('jsonAddress', infoAddressArr) ;
+
+                   // infoPriceArr += info[i].Price;
+                   // convo.setVar('jsonPrice', infoPriceArr)
+
+                  if (info[i].Adress == "Sofouli 24") {
+                       answerAdress += info[i].Adress; // Maybe I don't need it
+                       convo.setVar('jsonAddress', answerAdress);
+                       console.log("THIS IS: " + info[i].Adress);
+                       //convo.addMessage('Address Exist: {{vars.jsonAddress}} ..', 'default')
+                   }
+               }
+
+               convo.addMessage('Address: {{vars.jsonAddress}} ..', 'default');
+
+               convo.addMessage('First Price {{vars.jsonInfo}} ..', 'default');
+
+           })
+
+
+
+           // });  // end request(..)
+
+           bot.reply(message,'Hears JSON Clean.');
+
+
+           
+
+           // Start convertation about Address
+           bot.startConversation(message, function(err, convo) {
+
+               convo.addMessage('Give me Area', 'default')
+
+               // Add a question, to the deafult thread, with a response handler that will set a variable
+               // and then switch us to another thread
+               //var output=[];
+               convo.addQuestion('', function(response, convo) {
+                   convo.setVar('userAddress', response.text)
+                   //var newVar= response.text;
+                   // now the convo.vars field has been updated
+                   console.log('convo.vars:', convo.vars)
+                   //let j=0;
+                   for(var i=0; i<info.length; i++){
+                   if (response.text == info[i].Area) {
+                        //output[j]=info[i];
+                        //j++
+                       console.log("THIS IS: " + info[i].Area);
+                       console.log("THIS IS: " + info[i].Adress);
+                       console.log("THIS IS: " + info[i].Id);
+                       console.log("THIS IS: " + info[i].Price);
+                     //console.log(output[j]);
+                       convo.gotoThread('step2');
+                   }
+               }
+
+               },  {key: 'response'}, 'default')
+               
+
+               convo.addMessage('We have a property here','step2');
+
+
+               bot.startConversation(message, function(err, convo) {
+               
+               convo.addMessage('Give me Price', 'default')
+
+               // Add a question, to the deafult thread, with a response handler that will set a variable
+               // and then switch us to another thread
+               //var output=[];
+               convo.addQuestion('', function(response, convo) {
+                   convo.setVar('userPrice', response.text)
+                   //var newVar= response.text;
+                   // now the convo.vars field has been updated
+                   console.log('convo.vars:', convo.vars)
+                   //let j=0;
+                   for(var i=0; i<info.length; i++){
+                   if (response.text<=info[i].Price) {
+                        //output[j]=info[i];
+                        //j++
+                       console.log("THIS IS: " + info[i].Area);
+                       console.log("THIS IS: " + info[i].Adress);
+                       console.log("THIS IS: " + info[i].Id);
+                       console.log("THIS IS: " + info[i].Price);
+                     //console.log(output[j]);
+                       convo.gotoThread('step3');
+                   }
+               }
+
+               },  {key: 'response'}, 'default')
+
+               convo.addMessage('We have a property in this price range','step3');
+               
+               // Since this variable is defined after our response handler runs,
+               // it is available throughout our conversation after that point
+
+               /*convo.addMessage('Give me price', 'default')
+
+               convo.addQuestion('', function(response, convo) {
+                convo.setVar('userAddress', response.text)
+                //var newVar= response.text;
+                // now the convo.vars field has been updated
+                console.log('convo.vars:', convo.vars)
                 
+                for(var i=0; i<info.length; i++){
+                if (response.text == info[i].Area) {
 
-                console.log('error:', error); // Print the error if one occurred
-                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-                // console.log('body:', body); // Print the HTML for the Google homepage.
+                    console.log("THIS IS: " + info[i].Area);
+                    console.log("THIS IS: " + info[i].Adress);
+                    console.log("THIS IS: " + info[i].Id);
+                    console.log("THIS IS: " + info[i].Price);
 
-                // const info = JSON.parse(body);
-                convo.setVar('jsonInfo', info[0].Price)
-                // var infoAddressArr = '';
-                var infoPriceArr;
-
-
-                for(var i = 0; i < info.length; i++){
-
-                    infoAddressArr += info[i].Adress;
-                    convo.setVar('jsonAddress', infoAddressArr)
-
-                    // infoPriceArr += info[i].Price;
-                    // convo.setVar('jsonPrice', infoPriceArr)
-
-                    if (info[i].Adress == "Sofouli 24") {
-                        answerAdress += info[i].Adress; // Maybe I don't need it
-                        convo.setVar('jsonAddress', answerAdress)
-                        console.log("THIS IS: " + info[i].Adress);
-                        convo.addMessage('Address Exist: {{vars.answerAdress}} ..', 'default')
-                    }
+                    convo.gotoThread('step2');
                 }
+            }
 
-                convo.addMessage('Address: {{vars.jsonAddress}} ..', 'default')
+            },  {key: 'response'}, 'default') */
+        })
 
-                convo.addMessage('First Price {{vars.jsonInfo}} ..', 'default')
-                
-            })
+           })
 
-                
+       });
 
-            // });  // end request(..)
-        
-            bot.reply(message,'Hears JSON Clean.');      
-
-
-
-
-            // Start convertation about Address
-            bot.startConversation(message, function(err, convo) {
-
-                convo.addMessage('Give me Address', 'default')
-
-                // Add a question, to the deafult thread, with a response handler that will set a variable
-                // and then switch us to another thread
-                convo.addQuestion('', function(response, convo) {
-                    convo.setVar('userAddress', response.text)
-                    // now the convo.vars field has been updated
-                    console.log('convo.vars:', convo.vars)
-                    convo.gotoThread('step2')
-                },  {key: 'response'}, 'default')
-                
-                // Since this variable is defined after our response handler runs,
-                // it is available throughout our conversation after that point
-                convo.addMessage('User Address: {{vars.userAddress}}', 'step2')
-
-                // for(var i = 0; i < info.length; i++){
-                //     if (info[i].Adress == "Sofouli 24") {
-                //         answerAdress += info[i].Adress; // Maybe I don't need it
-                //         convo.setVar('jsonAddress', answerAdress)
-                //         console.log("THIS IS: " + info[i].Adress);
-                //         convo.addMessage('THIS IS: {{vars.userAddress}} ..', 'default')
-                //     }
-                // }    
-            })
-
-        });
-    
-    });
+   });
 }
---
+++ skills/user_input.js
@@ -0,0 +1,52 @@
+/* Gets user's input of area and informs the user if there are any properties in this area and output in the
+command prompt of all the properties of the houses in this area */
+
+const request = require('request');
+
+module.exports = function(controller) {
+
+
+   var mysql      = require('mysql');            // Database
+   var connection = mysql.createConnection({
+     host     : '127.0.0.1',
+     user     : 'root',
+     password : '',
+     database : 'kleindb'
+   })
+   connection.connect();
+
+
+
+   controller.hears('user input', 'message_received', function(bot, message) {
+
+
+          // Start convertation about Area
+          bot.startConversation(message, function(err, convo) {
+
+
+              convo.addQuestion('Give me area: ', function(response, convo) {
+
+               convo.setVar('area', response.text)
+
+                       connection.query("SELECT * FROM properties WHERE Area= ('" + response.text + "')" , function (error, results, fields) {
+                       if (error) throw error;
+                       console.log('The solution is: ', results);
+                       });
+
+                   console.log('convo.vars:', convo.vars)  // For debugging
+                   convo.gotoThread('step2');          // Goes to thread step2 and prints the input
+
+
+
+               },  {key: 'response'}, 'default')
+
+               convo.addMessage('This is your area: {{vars.area}}', 'step2')
+
+
+
+          })
+
+   });
+
+
+}
